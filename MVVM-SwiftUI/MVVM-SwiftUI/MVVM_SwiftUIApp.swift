327e0f41c2f13ded400040a8af377123c7ea82d1
Anand Yadav <anand.yadav@nagarro.com>
2024-05-19 16:28:23 +0530

    initial commit

---
+++ MVVM-SwiftUI/MVVM-SwiftUI/MVVM_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_SwiftUIApp.swift
+//  MVVM-SwiftUI
+//
+//  Created by Anand Yadav on 19-05-2024.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
