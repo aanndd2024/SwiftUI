75f5e0236025446058ecbc025b7b2ece21357d85
Anand Yadav <anand.yadav@nagarro.com>
2024-05-20 16:43:30 +0530

    Added details view

---
+++ MVVM-SwiftUI/MVVM-SwiftUI/Views/AsyncImageView.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncImageView.swift
+//  MVVM-SwiftUI
+//
+//  Created by Anand Yadav on 20-05-2024.
+//
+
+import SwiftUI
+
+struct AsyncImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncImageView()
+}
---
+++ MVVM-SwiftUI/MVVM-SwiftUI/Views/ProductDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  ProductDetailsView.swift
+//  MVVM-SwiftUI
+//
+//  Created by Anand Yadav on 20-05-2024.
+//
+
+import SwiftUI
+
+struct ProductDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductDetailsView()
+}
